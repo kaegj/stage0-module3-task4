7ef4021f926488df656e7cf14859b88ae3a2d771
kaegan <kkiefer4@student.umgc.edu>
2024-06-06 12:13:53 +0900

    Submission

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        System.out.println('c');
+        System.out.println('h');
+        System.out.println('a');
+        System.out.println('r');
     }
 }
